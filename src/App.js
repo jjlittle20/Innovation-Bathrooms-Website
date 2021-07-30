e4075d072a432ef084d15d8022f442848f985d96
jjlittle20 <66135577+jjlittle20@users.noreply.github.com>
2021-07-31 00:10:51 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
-import { Box, Container, Grid } from "@material-ui/core";
+import { Box, Container, Grid, Link } from "@material-ui/core";
 import ImageHero from "./img/hero.jpg";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
@@ -168,6 +168,70 @@ function App() {
             </Grid>
           </Grid>
         </Container>
+        <footer>
+          <Box
+            px={{ xs: 3, sm: 10 }}
+            py={{ xs: 5, sm: 10 }}
+            bgcolor="text.secondary"
+            color="white"
+          >
+            <Container maxWidth="lg">
+              <Grid container spacing={5}>
+                <Grid item xs={12} sm={4}>
+                  <Box borderBottom={1}>Help</Box>
+                  <Box>
+                    <Link href="/" color="inherit">
+                      FAQ
+                    </Link>
+                  </Box>
+                  <Box>
+                    <Link href="/" color="inherit">
+                      Testimonials
+                    </Link>
+                  </Box>
+                  <Box>
+                    <Link href="/" color="inherit">
+                      Suppliers
+                    </Link>
+                  </Box>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Box borderBottom={1}>Get A Quote</Box>
+                  <Box>
+                    <Link href="/" color="inherit">
+                      Contact Us
+                    </Link>
+                  </Box>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Box borderBottom={1}>Navigation</Box>
+                  <Box>
+                    <Link href="/" color="inherit">
+                      Home
+                    </Link>
+                  </Box>
+                  <Box>
+                    <Link href="/" color="inherit">
+                      Gallery
+                    </Link>
+                  </Box>
+                  <Box>
+                    <Link href="/" color="inherit">
+                      Terms and Conditions
+                    </Link>
+                  </Box>
+                </Grid>
+              </Grid>
+              <Box
+                textAlign="center"
+                pt={{ xs: 5, sm: 10 }}
+                pb={{ xs: 5, sm: 0 }}
+              >
+                Innovation Bathrooms&reg; {new Date().getFullYear()}
+              </Box>
+            </Container>
+          </Box>
+        </footer>
       </div>
     </body>
   );
